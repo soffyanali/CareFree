bd325541d6e24a43e41957a544a6d1e16986104e
Soffyan Ali <soffyanali@gmail.com>
2017-07-14 22:13:12 +0100

    Completed Map part
    Map key AIzaSyDjrURCUFOQq9YBD8bMSa35jNU7gocXwoE

--- CareFree/app/src/main/java/com/example/carefree/carefree/ITExpertMap.java
+++ CareFree/app/src/main/java/com/example/carefree/carefree/ITExpertMap.java
@@ -32,25 +32,257 @@ import android.Manifest;
 
 public class ITExpertMap extends AppCompatActivity
 {
-        double longitute;
-        double latitude;
-        ListView list;
-        Button updatelocation;
-        SharedPreferences preferences;
+               GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
-        @Override
-        public void onBackPressed() {
-            Intent intent = new Intent(ShowMapStandard.this, LoginActivity.class);
-            startActivity(intent);
-            finish();
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private GoogleApiClient mGoogleApiClient;
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private LocationRequest mLocationRequest;
+    private LocationSource.OnLocationChangedListener mListener;
+    double longitute;
+    double latitude;
+    ListView list;
+    Button updatelocation,chatProfpp;
+    SharedPreferences preferences;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.showmap_professional);
+
+        try {
+
+            setUpMapIfNeeded();
+
+            if (android.os.Build.VERSION.SDK_INT > 9) {
+                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                StrictMode.setThreadPolicy(policy);
+            }
+
+
+            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+            if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                // Toast.makeText(this, "GPS is Enabled in your devide", Toast.LENGTH_SHORT).show();
+            } else {
+                showGPSDisabledAlertToUser();
+            }
+
+
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+
+            mLocationRequest = LocationRequest.create()
+                    .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                    .setInterval(1 * 1000)        // 10 seconds, in milliseconds
+                    .setFastestInterval(500); // 1 second, in milliseconds
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.showmap_professional);
+        // Update login value it states that user is active
 
+        preferences = getSharedPreferences("loginusersdetails", MODE_PRIVATE);
+        String name = preferences.getString("name", null);
+        String password = preferences.getString("password", null);
+        String login = "y";
+
+        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        nameValuePairs.add(new BasicNameValuePair("name", name));
+        nameValuePairs.add(new BasicNameValuePair("password", password));
+        nameValuePairs.add(new BasicNameValuePair("login", login));
+
+        try {
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(ipadd+"updatelogindetails.php");
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpResponse response = httpClient.execute(httpPost);
+            HttpEntity entity = response.getEntity();
+            // Toast.makeText(getApplicationContext(), "Logout Successfully", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Error= " + e.getMessage(), Toast.LENGTH_LONG).show();
         }
+        //
+
+        chatProfpp= (Button) findViewById(R.id.chatProfpp);
+        chatProfpp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                preferences = getSharedPreferences("loginusersdetails", MODE_PRIVATE);
+                String name = preferences.getString("name", null);
+                Intent i=new Intent(ShowMapProfessional.this,ChatUserListProf.class);
+                i.putExtra("sharename",name);
+                startActivity(i);
+                finish();
+             }
+        });
+
+        updatelocation = (Button) findViewById(R.id.updatelocation);
+        updatelocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                preferences = getSharedPreferences("loginusersdetails", MODE_PRIVATE);
+                String name = preferences.getString("name", null);
+                String password = preferences.getString("password", null);
+
+
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+                nameValuePairs.add(new BasicNameValuePair("name", name));
+                nameValuePairs.add(new BasicNameValuePair("password", password));
+                nameValuePairs.add(new BasicNameValuePair("userlatitute", String.valueOf(latitude)));
+                nameValuePairs.add(new BasicNameValuePair("userlongitute", String.valueOf(longitute)));
+
+                try {
+                    HttpClient httpClient = new DefaultHttpClient();
+                    HttpPost httpPost = new HttpPost(ipadd+"updatecurrentlocation.php");
+                    httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                    HttpResponse response = httpClient.execute(httpPost);
+                    HttpEntity entity = response.getEntity();
+                    Toast.makeText(getApplicationContext(), "Current location updated", Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "Error= " + e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected()) {
+            FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    private void showGPSDisabledAlertToUser() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("GPS is disabled in your device. Would you like to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Goto Settings Page To Enable GPS",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Intent callGPSSettingIntent = new Intent(
+                                        android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                startActivity(callGPSSettingIntent);
+                            }
+                        });
+        alertDialogBuilder.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+    }
+
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapprofessional)).
+                    getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap() {
+
+        mMap.getUiSettings().setZoomGesturesEnabled(true);
+    }
+
+
+    public void onConnected(Bundle bundle) {
+        Location location = null;
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        } else {
+            // requestPermissions(new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+            Toast.makeText(this, "Should ask for the approval/denial here", Toast.LENGTH_LONG).show();
+        }
+
+        //Location location = FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (location == null) {
+            FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        } else {
+            handleNewLocation(location);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void handleNewLocation(Location location) {
+        final double currentLatitude = location.getLatitude();
+        final double currentLongitude = location.getLongitude();
+
+        preferences = getSharedPreferences("loginusersdetails", MODE_PRIVATE);
+        String name = preferences.getString("name", null);
+
+        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+        MarkerOptions options = new MarkerOptions()
+                .position(latLng)
+                .title(name.toUpperCase() + " you are here!");
+        mMap.addMarker(options);
+        float zoomLevel = 10.0f; //This goes up to 21
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(12));
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        try {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    public void requestPermissions(@NonNull String[] permissions, int requestCode)
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for Activity#requestPermissions for more details.
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+        }catch (Exception e)
+        {}
+        //Toast.makeText(WelcomeScreen.this,"Handle new Location = "+location.getLatitude()+" Langi= "+ location.getLongitude(),Toast.LENGTH_SHORT).show();
+        longitute= location.getLongitude();
+        latitude=location.getLatitude();
+
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
 
 }
